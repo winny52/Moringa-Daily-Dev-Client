2ad4aea094c3adb15861c2bb287290798e52a041
mitunda <peterbrian28@gmail.com>
2023-11-07 14:28:38 +0300

    created function to handle random content posting

--- moringa-daily/src/components/TrendingMedia.jsx
+++ moringa-daily/src/components/TrendingMedia.jsx
@@ -13,7 +13,7 @@ const TrendingMedia = () => {
   useEffect(() => {
     async function fetchTrendingMedia() {
       try {
-        const response = await api.get("/trending-media");
+        const response = await api.get("/content/random"); // Replace with the actual endpoint
 
         // Set the fetched trending media data to the state
         setTrendingMedia(response.data);
@@ -26,32 +26,32 @@ const TrendingMedia = () => {
   }, []); // The empty dependency array ensures this runs once when the component mounts
 
   return (
-    <div className="text-white p-12 grid grid-cols-2">
-      <div className="col-span-1 p-12">
-        <div className="flex items-center gap-3 mb-4">
-          <div className="w-12 h-12 rounded-full bg-default-gold flex justify-center items-center text-default-green">
+    <div className='text-white p-12 grid grid-cols-2'>
+      <div className='col-span-1 p-12'>
+        <div className='flex items-center gap-3 mb-4'>
+          <div className='w-12 h-12 rounded-full bg-default-gold flex justify-center items-center text-default-green'>
             <StarIcon style={{ fontSize: "28px" }} />
           </div>
-          <h5 className="text-default-green text-lg font-semibold my-auto">
+          <h5 className='text-default-green text-lg font-semibold my-auto'>
             Trending Media
           </h5>
         </div>
-        <h2 className="text-gray-700 text-3xl py-3">
+        <h2 className='text-gray-700 text-3xl py-3'>
           {trendingMedia?.title || "Loading..."}
         </h2>
-        <hr className="w-48" />
-        <p className="py-3 text-gray-600 text-sm">
+        <hr className='w-48' />
+        <p className='py-3 text-gray-600 text-sm'>
           {trendingMedia?.description || "Loading..."}
         </p>
       </div>
-      <div className="col-span-1 border rounded p-12 flex items-center justify-center bg-slate-200">
+      <div className='col-span-1 border rounded p-12 flex items-center justify-center bg-slate-200'>
         <iframe
-          width="350"
-          height="200"
-          src={trendingMedia?.media_url || "https://www.youtube.com/embed/your-default-video"}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          width='350'
+          height='200'
+          src={trendingMedia?.video_url || "https://www.youtube.com/embed/your-default-video"}
+          title='YouTube video player'
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
           allowFullScreen
         ></iframe>
       </div>
--- moringa-daily/src/components/VideoSlider.jsx
+++ moringa-daily/src/components/VideoSlider.jsx
@@ -1,21 +1,72 @@
-import React from "react";
-import VideocamIcon from "@mui/icons-material/Videocam";
-import VideoCard from "./VideoCard";
+// import React from "react";
+// import VideocamIcon from "@mui/icons-material/Videocam";
+// import VideoCard from "./VideoCard";
+
+// const VideoSlider = () => {
+//   return (
+//     <div className='bg-default-green px-16 py-12'>
+//       <div className='flex gap-3 items-center'>
+//         <div className='w-12 h-12 rounded-full bg-default-gold flex justify-center items-center text-default-green'>
+//           <VideocamIcon style={{ fontSize: "28px" }} />
+//         </div>
+//         <h5 className='text-white text-lg font-semibold my-auto'>
+//           Popular Videos
+//         </h5>
+//       </div>
+//       <div className='mt-3 flex justify-center'>
+//         <VideoCard />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default VideoSlider;
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+// Create an Axios instance with the base URL of your local backend
+const api = axios.create({
+  baseURL: "http://127.0.0.1:5000", // Replace with your local backend URL
+});
 
 const VideoSlider = () => {
+  const [highestRatedContent, setHighestRatedContent] = useState(null);
+
+  useEffect(() => {
+    async function fetchHighestRatedContent() {
+      try {
+        const response = await api.get("/highest-rated-content"); // Make an API request using the Axios instance
+        setHighestRatedContent(response.data);
+      } catch (error) {
+        console.error("Error fetching highest-rated content:", error);
+      }
+    }
+
+    fetchHighestRatedContent();
+  }, []);
+
   return (
-    <div className='bg-default-green px-16 py-12'>
-      <div className='flex gap-3 items-center'>
-        <div className='w-12 h-12 rounded-full bg-default-gold flex justify-center items-center text-default-green'>
-          <VideocamIcon style={{ fontSize: "28px" }} />
+    <div className="bg-default-green px-16 py-12">
+      <div className="flex gap-3 items-center">
+        <div className="w-12 h-12 rounded-full bg-default-gold flex justify-center items-center text-default-green">
+          {/* Display the icon */}
+          {/* ... */}
         </div>
-        <h5 className='text-white text-lg font-semibold my-auto'>
-          Popular Videos
-        </h5>
+        <h5 className="text-white text-lg font-semibold my-auto">Popular Content</h5>
       </div>
-      <div className='mt-3 flex justify-center'>
-        <VideoCard />
-      </div>
+      {highestRatedContent ? (
+        <div className="mt-3 flex justify-center">
+          {/* Display the highest-rated content */}
+          <h2 className="text-gray-700 text-3xl py-3">{highestRatedContent.title}</h2>
+          <hr className="w-48" />
+          <p className="py-3 text-gray-600 text-sm">{highestRatedContent.description}</p>
+        </div>
+      ) : (
+        <div className="mt-3 flex justify-center">
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 };
