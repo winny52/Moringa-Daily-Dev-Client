536dc19def13d517e2ceb3e39fadbd2e2c2b5a0d
mitunda <peterbrian28@gmail.com>
2023-11-07 20:17:31 +0300

    cloudinary mock

--- moringa-daily/src/App.js
+++ moringa-daily/src/App.js
@@ -3,21 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import ArticlePage from "./pages/ArticlePage";
-import Dashboard from "./pages/Dashboard";
+// import Upload from "./components/dashboard/Upload"; // Update the import
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/" element={<HomePage />} /> {/* Render HomePage as the default route */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/articles/:id" element={<ArticlePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Upload />} /> Use the updated import */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
--- moringa-daily/src/components/dashboard/CreatePost.jsx
+++ moringa-daily/src/components/dashboard/CreatePost.jsx
@@ -1,28 +1,27 @@
 import React from "react";
+import Upload from "./components/dashboard/Upload"; // Import the Upload component
 
 const CreatePost = () => {
   return (
-    <div className='bg-white p-4 shadow-sm mx-2 mt-4'>
-      <div className='flex items-center gap-3 mt-3'>
-        <div className='w-4 h-4 rounded-full bg-red-900 flex justify-center items-center'></div>
-        <h6 className='text-red-900 my-auto text-xl'>Post Content</h6>
+    <div className="bg-white p-4 shadow-sm mx-2 mt-4">
+      <div className="flex items-center gap-3 mt-3">
+        <div className="w-4 h-4 rounded-full bg-red-900 flex justify-center items-center"></div>
+        <h6 className="text-red-900 my-auto text-xl">Post Content</h6>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-5'>
-        <div className='col-span-1 md:col-span-3'>
-          <div className='mb-3'>
-            <p className='text-gray-600 text-sm py-2'>Post Title below</p>
+      <div className="grid grid-cols-1 md:grid-cols-5">
+        <div className="col-span-1 md:col-span-3">
+          <div className="mb-3">
+            <p className="text-gray-600 text-sm py-2">Post Title below</p>
             <input
-              type='text'
-              className='w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600'
+              type="text"
+              className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
             />
           </div>
-          <div className='mb-3'>
-            <p className='text-gray-600 text-sm py-2'>
-              Choose story categories
-            </p>
+          <div className="mb-3">
+            <p className="text-gray-600 text-sm py-2">Choose story categories</p>
             <select
-              type='text'
-              className='w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600'
+              type="text"
+              className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
             >
               <option>A.I & Machine Learning</option>
               <option>Internet of Things (IoT)</option>
@@ -30,50 +29,46 @@ const CreatePost = () => {
               <option>Theory of Computation</option>
             </select>
           </div>
-          <div className='mb-3'>
-            <p className='text-gray-600 text-sm py-2'>
+          <div className="mb-3">
+            <p className="text-gray-600 text-sm py-2">
               Type the full article in the box below
             </p>
             <textarea
-              type='text'
+              type="text"
               rows={8}
-              className='w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600'
+              className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
             ></textarea>
           </div>
         </div>
-        <div className='col-span-1 md:col-span-2 mx-8'>
-          <div className='mb-3 content-type'>
-            <h6 className='text-default-gold py-2 text-md'>
-              Select Content Type
-            </h6>
-            <div className='flex gap-2 items-center '>
-              <input type='radio' name='content_type' />
-              <p className='text-gray-600 text-md'>Article</p>
+        <div className="col-span-1 md:col-span-2 mx-8">
+          <div className="mb-3 content-type">
+            <h6 className="text-default-gold py-2 text-md">Select Content Type</h6>
+            <div className="flex gap-2 items-center">
+              <input type="radio" name="content_type" />
+              <p className="text-gray-600 text-md">Article</p>
             </div>
-            <div className='flex gap-2 items-center '>
-              <input type='radio' name='content_type' />
-              <p className='text-gray-600 text-md'>Audio</p>
+            <div className="flex gap-2 items-center">
+              <input type="radio" name="content_type" />
+              <p className="text-gray-600 text-md">Audio</p>
             </div>
-            <div className='flex gap-2 items-center '>
-              <input type='radio' name='content_type' />
-              <p className='text-gray-600 text-md'>Video</p>
+            <div className="flex gap-2 items-center">
+              <input type="radio" name="content_type" />
+              <p className="text-gray-600 text-md">Video</p>
             </div>
           </div>
-          <div className='mb-3'>
-            <h6 className='text-default-gold py-2 text-md'>
-              Upload Features Image
-            </h6>
-            <input type='file' className='rounded py-1' />
+          <div className="mb-3">
+            <h6 className="text-default-gold py-2 text-md">Upload Features Image</h6>
+            <Upload /> {/* Render the Upload component here */}
           </div>
-          <div className='mb-3'>
-            <h6 className='text-default-gold py-2 text-md'>Enter Link URL:</h6>
+          <div className="mb-3">
+            <h6 className="text-default-gold py-2 text-md">Enter Link URL:</h6>
             <input
-              type='text'
-              className='w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600'
-              placeholder=' *For Audio and video files'
+              type="text"
+              className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
+              placeholder="*For Audio and video files"
             />
           </div>
-          <button className='mt-3 w-full bg-red-900 uppercase text-md text-white py-1 rounded mx-2'>
+          <button className="mt-3 w-full bg-red-900 uppercase text-md text-white py-1 rounded mx-2">
             Publish Post
           </button>
         </div>
@@ -83,3 +78,47 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
+
+
+
+// import React, { useEffect, useRef } from "react";
+
+// const CreatePost = () => {
+//   const widgetRef = useRef();
+
+//   useEffect(() => {
+//     const cloudinaryRef = window.cloudinary;
+//     widgetRef.current = cloudinaryRef.createUploadWidget(
+//       {
+//         cloudName: "dkwurmldt",
+//         uploadPreset: "your-upload-preset",
+//       },
+//       (error, result) => {
+//         if (!error && result && result.event === "success") {
+//           // Handle the uploaded image here
+//           console.log("Upload success. Public ID: ", result.info.public_id);
+//         }
+//       }
+//     );
+//   }, []);
+
+//   const handleImageUpload = () => {
+//     widgetRef.current.open(); // Opens the Cloudinary upload widget
+//   };
+
+//   return (
+//     <div className='bg-white p-4 shadow-sm mx-2 mt-4'>
+//       {/* ... your existing JSX ... */}
+//       <div className='mb-3'>
+//         <h6 className='text-default-gold py-2 text-md'>Upload Features Image</h6>
+//         <button onClick={handleImageUpload} className='rounded py-1'>
+//           Upload Image
+//         </button>
+//       </div>
+//       {/* ... more of your JSX ... */}
+//     </div>
+//   );
+// };
+
+// export default CreatePost;
+
