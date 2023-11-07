71c82d607312c5e9b142d234cef106d4e62dcdc6
Winny <winny.chelangat2@student.moringaschool.com>
2023-11-07 09:22:28 +0300

    Articlepage removed

--- moringa-daily/src/components/dashboard/NewArticles.jsx
+++ moringa-daily/src/components/dashboard/NewArticles.jsx
@@ -6,7 +6,7 @@ const NewArticles = () => {
     <div className='text-white shadow-sm p-4 mx-2 mt-3'>
       <div className='flex items-center gap-3 my-3'>
         <div className='w-4 h-4 rounded-full bg-red-500 flex justify-center items-center'></div>
-        <h6 className='text-red-500 my-auto'>Approve New Articles</h6>
+        <h6 className='text-red-500 my-auto'>Approve Flagged Articles</h6>
       </div>
       <div className='grid grid-cols-3 flex justify-center'>
         <div className='col-span-1 p-2'>
