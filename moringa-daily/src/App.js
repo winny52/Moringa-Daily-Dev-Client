9b35964920d1ca92993e1a9899b7565faccff2f6
mitunda <peterbrian28@gmail.com>
2023-11-08 21:41:18 +0300

    edited the UploadImage component

--- moringa-daily/src/App.js
+++ moringa-daily/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import ArticlePage from "./pages/ArticlePage";
-// import Upload from "./components/dashboard/Upload"; // Update the import
+import CreatePost from "./components/dashboard/CreatePost"; // Updated import for CreatePost component
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/articles/:id" element={<ArticlePage />} />
-        {/* <Route path="/dashboard" element={<Upload />} /> Use the updated import */}
+        <Route path="/dashboard" element={<CreatePost />} /> {/* Updated route */}
       </Routes>
     </Router>
   );
--- moringa-daily/src/components/dashboard/CreatePost.jsx
+++ moringa-daily/src/components/dashboard/CreatePost.jsx
@@ -1,7 +1,102 @@
-import React from "react";
-import Upload from "./components/dashboard/Upload"; // Import the Upload component
+// import React from "react";
+// import ImageUpload from "./ImageUpload"; // Import the Upload component from the same directory
+
+// const CreatePost = () => {
+//   return (
+//     <div className="bg-white p-4 shadow-sm mx-2 mt-4">
+//       <div className="flex items-center gap-3 mt-3">
+//         <div className="w-4 h-4 rounded-full bg-red-900 flex justify-center items-center"></div>
+//         <h6 className="text-red-900 my-auto text-xl">Post Content</h6>
+//       </div>
+//       <div className="grid grid-cols-1 md:grid-cols-5">
+//         <div className="col-span-1 md:col-span-3">
+//           <div className="mb-3">
+//             <p className="text-gray-600 text-sm py-2">Post Title below</p>
+//             <input
+//               type="text"
+//               className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
+//             />
+//           </div>
+//           <div className="mb-3">
+//             <p className="text-gray-600 text-sm py-2">Choose story categories</p>
+//             <select
+//               type="text"
+//               className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
+//             >
+//               <option>A.I & Machine Learning</option>
+//               <option>Internet of Things (IoT)</option>
+//               <option>Cloud Computing</option>
+//               <option>Theory of Computation</option>
+//             </select>
+//           </div>
+//           <div className="mb-3">
+//             <p className="text-gray-600 text-sm py-2">
+//               Type the full article in the box below
+//             </p>
+//             <textarea
+//               type="text"
+//               rows={8}
+//               className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
+//             ></textarea>
+//           </div>
+//         </div>
+//         <div className="col-span-1 md:col-span-2 mx-8">
+//           <div className="mb-3 content-type">
+//             <h6 className="text-default-gold py-2 text-md">Select Content Type</h6>
+//             <div className="flex gap-2 items-center">
+//               <input type="radio" name="content_type" />
+//               <p className="text-gray-600 text-md">Article</p>
+//             </div>
+//             <div className="flex gap-2 items-center">
+//               <input type="radio" name="content_type" />
+//               <p className="text-gray-600 text-md">Audio</p>
+//             </div>
+//             <div className="flex gap-2 items-center">
+//               <input type="radio" name="content_type" />
+//               <p className="text-gray-600 text-md">Video</p>
+//             </div>
+//           </div>
+//           <div className="mb-3">
+//             <h6 className="text-default-gold py-2 text-md">Upload Features Image</h6>
+//             <ImageUpload/> {/* Render the Upload component here */}
+//           </div>
+//           <div className="mb-3">
+//             <h6 className="text-default-gold py-2 text-md">Enter Link URL:</h6>
+//             <input
+//               type="text"
+//               className="w-full py-1 border focus:outline-none rounded-lg px-2 text-gray-600"
+//               placeholder="*For Audio and video files"
+//             />
+//           </div>
+//           <button className="mt-3 w-full bg-red-900 uppercase text-md text-white py-1 rounded mx-2">
+//             Publish Post
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CreatePost;
+
+
+import React, { useState, useEffect } from "react";
+import ImageUpload from "./ImageUpload"; // Import the Upload component from the same directory
+import { Image } from 'cloudinary-react'; // Import Image component from Cloudinary
 
 const CreatePost = () => {
+  // Step 1: Define state variables
+  const [imageURL, setImageURL] = useState(""); // State variable to store the URL of the uploaded image
+
+  // Step 2: Callback function to handle the image URL
+  const handleImageURL = (url) => {
+    console.log("Image handler reached");
+    setImageURL(url);
+    console.log("My Image");
+    console.log(imageURL)
+  };
+   
+
   return (
     <div className="bg-white p-4 shadow-sm mx-2 mt-4">
       <div className="flex items-center gap-3 mt-3">
@@ -58,7 +153,8 @@ const CreatePost = () => {
           </div>
           <div className="mb-3">
             <h6 className="text-default-gold py-2 text-md">Upload Features Image</h6>
-            <Upload /> {/* Render the Upload component here */}
+            {/* Step 3: Pass the callback function to ImageUpload */}
+            <ImageUpload onImageUpload={handleImageURL} />
           </div>
           <div className="mb-3">
             <h6 className="text-default-gold py-2 text-md">Enter Link URL:</h6>
@@ -73,52 +169,19 @@ const CreatePost = () => {
           </button>
         </div>
       </div>
+      {imageURL && ( // Step 4: Render the image if imageURL is available
+
+        <div className="mb-3">
+          <h1>{imageURL}</h1>
+        
+
+          <h6 className="text-default-gold py-2 text-md">Uploaded Image</h6>
+          <Image cloudName={process.env.REACT_APP_CLOUDINARY_CLOUD_NAME} publicId={imageURL} />
+        </div>
+      )}
     </div>
   );
 };
 
 export default CreatePost;
-
-
-
-// import React, { useEffect, useRef } from "react";
-
-// const CreatePost = () => {
-//   const widgetRef = useRef();
-
-//   useEffect(() => {
-//     const cloudinaryRef = window.cloudinary;
-//     widgetRef.current = cloudinaryRef.createUploadWidget(
-//       {
-//         cloudName: "dkwurmldt",
-//         uploadPreset: "your-upload-preset",
-//       },
-//       (error, result) => {
-//         if (!error && result && result.event === "success") {
-//           // Handle the uploaded image here
-//           console.log("Upload success. Public ID: ", result.info.public_id);
-//         }
-//       }
-//     );
-//   }, []);
-
-//   const handleImageUpload = () => {
-//     widgetRef.current.open(); // Opens the Cloudinary upload widget
-//   };
-
-//   return (
-//     <div className='bg-white p-4 shadow-sm mx-2 mt-4'>
-//       {/* ... your existing JSX ... */}
-//       <div className='mb-3'>
-//         <h6 className='text-default-gold py-2 text-md'>Upload Features Image</h6>
-//         <button onClick={handleImageUpload} className='rounded py-1'>
-//           Upload Image
-//         </button>
-//       </div>
-//       {/* ... more of your JSX ... */}
-//     </div>
-//   );
-// };
-
-// export default CreatePost;
 
